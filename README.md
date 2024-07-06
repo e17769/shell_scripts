5e8c6a92a63ead74aa2c04319c7171cc6e4d0918
Ahmed F Gadir <e17769@hotmail.com>
2024-07-05 23:33:04 -0400

    refactor and rename files

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # shell_scripts
- Repo of all the cool SH file I have
+
+Repo of all the cool SH file I have
+
+## Examples of using the shell_scripts
+
+### delete empty folders
+
+sh delete-empty-folders.sh ~/Documents/GitHub/link-service/link-service/shell_scripts/images/emptydir
+
+### delete Cleal desktop folder
--- images/delete-empty-folders.sh
+++ images/delete-empty-folders.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
 
+#Example of use:
+# sh delete-empty-folders.sh ~/Documents/GitHub/link-service/link-service/shell_scripts/images/emptydir
+
+# Check if a string argument is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 <string>"
+    exit 1
+fi
+
+# Define color codes
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+
+# Print the input string
+echo -e "${YELLOW}$1${BLUE}"
+
 # Set the directory to check
-main_dir="Main"
+#main_dir= "$1"
+
+# view alll files
+find "$1" -type d -empty
 
 # Find all empty directories within the "Main" directory recursively
-find "$main_dir" -type d -empty -delete
+#find "$1" -type d -empty -delete
 
 # Print a message to indicate completion
-echo "Empty directories within '$main_dir' have been deleted."
+echo -e "${GREEN}Empty directories within '$1' have been deleted.${NC}"
